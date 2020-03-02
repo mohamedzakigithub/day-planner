fb17e4288108660bd8314a4d825ca5744d1de847
Mohamed Eleraky <mohamedzaki@hotmail.com>
2020-03-03 00:03:30 +1100

    add code comments

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+// Attempt to load tasks object from local storage or return an empty object if no locally stored object is found. 
 var tasks = JSON.parse(localStorage.getItem("tasks")) || {};
+// set interval to display data every one minuit. 
 var interval = setInterval(displayData, 60000);
+// call displayData function when the page loads. 
 displayData();
 
+// displayData function checks the current time and compares it to the end of working day time (6 PM) and displays todays
+// date into the current date element and calls the renderTable function or display a message and clear the Interval.
 
 function displayData() {
   var dateTime = moment();
@@ -16,6 +21,11 @@ function displayData() {
   }
 }
 
+// The renderTable function select all the text input elements with a class of description and checks for the presence 
+// of a key in the tasks object retrieved from local storage and used using the data-index attribute of each selected element
+// and sets the value to that item. The function then sets the time block color by setting the class (past, present or future)
+// by calling the checkTime function. 
+
 function renderTable() {
   var timeBlocks = $(".description");
   timeBlocks.each(function(i) {
@@ -27,6 +37,12 @@ function renderTable() {
     $(timeBlocks[i]).addClass(checkTime(dataIndex));
   });
 }
+
+// The check time function is called by the renderTable function on each text input element. 
+// The function compares the time value of the div to the hours of the current time by parsing the data-index 
+// value to a monemnt,js object and setting the format to ("HH-mma"). The output of the subtraction operation
+// is then examined and a switch case statement is used to return a string to be used as a class to change the 
+// color of the time block.
 
 function checkTime(timeBlock) {
   var currentHour = moment().hours();
@@ -44,6 +60,10 @@ function checkTime(timeBlock) {
       break;
   }
 }
+
+// Click Event handler of the save button assigned to each time block containing a callback function that sets the key and 
+// the data of the tasks object by capturing the text input value corresponding to the button's data-index attribute 
+// and then writes the object to local storage. 
 
 $(".saveBtn").on("click", function() {
   key = $(this).attr("data-index");
